4825eb9424a2e69757bbf95dacb9d6c0a4cb280d
Mahajannikita24 <mahajannikita2424@gmail.com>
2025-05-23 16:13:54 +0530

    initial login signup

--- team-ZenithX/frontend/Emergencio_App/App.js
+++ team-ZenithX/frontend/Emergencio_App/App.js
@@ -1,6 +1,25 @@
 import React from 'react';
-import AppNavigator from './src/AppNavigator';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: '#d32f2f',
+          tabBarInactiveTintColor: 'gray',
+          headerShown: false,
+        }}
+      >
+        <Tab.Screen name="Login" component={LoginScreen} />
+        <Tab.Screen name="Signup" component={SignupScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
--- team-ZenithX/frontend/Emergencio_App/src/AppNavigator.js
+++ team-ZenithX/frontend/Emergencio_App/src/AppNavigator.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import MapScreen from './screens/MapScreen';
+import LoginScreen from './screens/loginscreen';
+import SignupScreen from './screens/signupscreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +14,8 @@ export default function AppNavigator() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Map" component={MapScreen} />
+        <Stack.Screen name="Login" component={LoginScreen}/>
+        <Stack.Screen name="Signup" component={SignupScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
