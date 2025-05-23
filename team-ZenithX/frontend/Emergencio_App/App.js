eecf96289d8bfc7c0ff653f8c2390c67e73b7110
Mahajannikita24 <mahajannikita2424@gmail.com>
2025-05-23 16:14:37 +0530

    initial login signup

--- team-ZenithX/frontend/Emergencio_App/App.js
+++ team-ZenithX/frontend/Emergencio_App/App.js
@@ -1,25 +1,7 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AppNavigator from './src/AppNavigator';
 
-import LoginScreen from './screens/LoginScreen';
-import SignupScreen from './screens/SignupScreen';
-
-const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{
-          tabBarActiveTintColor: '#d32f2f',
-          tabBarInactiveTintColor: 'gray',
-          headerShown: false,
-        }}
-      >
-        <Tab.Screen name="Login" component={LoginScreen} />
-        <Tab.Screen name="Signup" component={SignupScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+  return <AppNavigator />;
 }
